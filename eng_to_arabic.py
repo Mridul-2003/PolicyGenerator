343c0b8f7c870beca3c4b25537483df16bc5205f
Mridul-2003 <110475111+Mridul-2003@users.noreply.github.com>
2025-02-06 15:33:08 +0530

    Add files via upload

---
+++ eng_to_arabic.py
@@ -0,0 +1,17 @@
+from googletrans import Translator
+import googletrans
+import asyncio
+class EngToArabic:
+	def __init__(self):
+		self.translator = Translator()
+	async def translate(self, text):
+		translation = await self.translator.translate(text, dest='ar')
+		return translation.text
+# print(googletrans.LANGUAGES)
+# import asyncio
+
+# async def translate_text():
+# 	translation = await translator.translate("Hello", dest='ar')
+# 	print(translation.text)
+
+# asyncio.run(translate_text())
