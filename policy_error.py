f67cb9eb8cf7eebc193d34e019a076879c36226b
Mridul-2003 <110475111+Mridul-2003@users.noreply.github.com>
2025-02-08 17:11:22 +0530

    Update policy_error.py

--- policy_error.py
+++ policy_error.py
@@ -1,12 +1,15 @@
 from flask import Flask, request, jsonify, Response
 import ollama
+from eng_to_arabic import EngToArabic
+from eng_to_arabic import ArabicToEng
+import asyncio
 ollama.pull('llama3.2:1b')
 app = Flask(__name__)
 
 # Dictionary to store conversation history for each user
 user_histories = {}
 
-def chat(msg,user_id):
+def chat(msg,user_id,language):
     # System prompt for AI behavior
     system_prompt = """You are an experienced advocate that knows about every policy 
     and can easily list down the errors by seeing the policies given by users. 
@@ -28,16 +31,31 @@ def chat(msg,user_id):
         token = partial_resp["message"]["content"]
         message += token
         print(message)
-
+    print(message)
     # Add AI response to history
     user_histories[user_id].append({"role": "assistant", "content": message})
+    if language == "arabic":
+            async def main(policy):
+                engtoarabic = EngToArabic()
+                return await engtoarabic.translate(policy)
+            translated_message = asyncio.run(main(message))
+            print(translated_message)
+            return {"reply":translated_message}
     return {"reply":message}
 @app.route('/chat', methods=['POST'])
 def policy_chat():
     data = request.get_json()
     user_id = data.get('user_id')  # Unique identifier for the user
     msg = data.get('msg')
-    return Response(chat(user_id,msg),mimetype="text/event-stream")  # Streaming response
+    language = data.get('language')
+
+    if language == "arabic":
+            async def main(policy):
+                arabictoenglish = ArabicToEng()
+                return await arabictoenglish.translate(policy)
+            msg = asyncio.run(main(msg))
+    reply = chat(msg,user_id,language)
+    return jsonify(reply)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True,port=8000)
